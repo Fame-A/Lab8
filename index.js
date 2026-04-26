d298473f7cdfcae12159bb71955607dfca61a153
Fame-A <ambonifame@gmail.com>
2026-04-26 14:46:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-const express = require('express');
-const sequelize = require('./config/database');
-const logger = require('./middleware/logger');
+const Student = require('./Student');
+const Course = require('./Course');
+const Enrollment = require('./Enrollment');
 
-require('./models/');
+//One-to-Many
+Student.belongsTo(Course, { foreignKey: 'courseId', as: 'course' });
+Course.hasMany(Student, { foreignKey: 'courseId', as: 'students' });
 
-const studentRoutes = require('./routes/students');
-const courseRoutes = require('./routes/courses');
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-app.use(logger);
-
-//Middleware
-app.use(express.json());
-
-//Routes
-app.use('/students', studentRoutes);
-app.use('/courses', courseRoutes);
-
-//Start the server
-app.get('/', (req, res) => {
-    res.json({ message: 'API is running' });
+//Many-to-Many
+Student.belongsToMany(Course, {
+    through: Enrollment,
+    foreignKey: 'studentId',
+    as : 'enrolledCourses'
 });
 
-//DB + Server start
-sequelize.sync()
-    .then(() => {
-        console.log('Database connected');
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost: ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('DB Error: ', err);
-    });
-
-
-app.use((req, res) => {
-    res.status(404).json({ error: 'Route not Found' });
+Course.belongsToMany(Student, {
+    through: Enrollment,
+    foreignKey: 'courseId',
+    as : 'enrolledStudents'
 });
-    
-app.use((err, req, res) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'An unexpected error occurred' });
-});
+
+module.exports = { Student, Course, Enrollment };
